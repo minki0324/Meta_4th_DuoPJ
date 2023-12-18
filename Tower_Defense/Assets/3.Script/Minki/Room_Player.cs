6fe463338336df847e931c09436f77260eb8b4f2
minki0324 <147981201+minki0324@users.noreply.github.com>
2023-12-18 18:40:04 +0900

    [Init] Network Setting
    네트워크 게임씬까지 연동

---
+++ Tower_Defense/Assets/3.Script/Minki/Room_Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Room_Player : NetworkRoomPlayer
+{
+    public override void OnGUI()
+    {
+
+    }
+
+}
