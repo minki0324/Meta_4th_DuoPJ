a9e9db6e2af3991141876c252fb31b2067d5426d
민준 <129065548+Nangdi@users.noreply.github.com>
2023-12-27 11:59:21 +0900

    [Fix] Invisible Test

--- Tower_Defense/Assets/3.Script/Minjun/ScanTower.cs
+++ Tower_Defense/Assets/3.Script/Minjun/ScanTower.cs
@@ -6,21 +6,34 @@ public class ScanTower : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Invisible"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Monster"))
         {
-            Debug.Log("인비들어와용");
-            IvisibleControl ivisibleControl = other.transform.root.GetComponent<IvisibleControl>();
-        // ivisibleControl이 null이 아닌 경우 DisInvisible() 메서드를 호출하고, 그렇지 않으면 아무것도 수행하지 않습니다.
-            ivisibleControl?.DisInvisible();
+            Monster_Control mon = other.GetComponent<Monster_Control>();
+            if (mon.state.type == MonsterState.monType.Invisible)
+            {
+
+                Debug.Log("인비들어와용");
+                IvisibleControl ivisibleControl = other/*.transform.root*/.GetComponent<IvisibleControl>();
+                mon.isInvi = false;
+                Debug.Log(ivisibleControl);
+                // ivisibleControl이 null이 아닌 경우 DisInvisible() 메서드를 호출하고, 그렇지 않으면 아무것도 수행하지 않습니다.
+                ivisibleControl?.DisInvisible();
+            }
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Invisible"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Monster"))
         {
-            IvisibleControl ivisibleControl = other.transform.root.GetComponent<IvisibleControl>();
-            // ivisibleControl이 null이 아닌 경우 DisInvisible() 메서드를 호출하고, 그렇지 않으면 아무것도 수행하지 않습니다.
-            ivisibleControl?.OnInvisible();
+
+            Monster_Control mon = other.GetComponent<Monster_Control>();
+            if (mon.state.type == MonsterState.monType.Invisible)
+            {
+                IvisibleControl ivisibleControl = other/*.transform.root*/.GetComponent<IvisibleControl>();
+                mon.isInvi = true;
+                // ivisibleControl이 null이 아닌 경우 DisInvisible() 메서드를 호출하고, 그렇지 않으면 아무것도 수행하지 않습니다.
+                ivisibleControl?.OnInvisible();
+            }
         }
     }
 }
